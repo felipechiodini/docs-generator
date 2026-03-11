c92d1ad7129f32f1ee210d8e1623c7934a5a8832
Felipe Bona <felipechiodinibona@hotmail.com>
2026-03-10 23:02:11 -0300

    save

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://felipechiodini.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs-generator',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
